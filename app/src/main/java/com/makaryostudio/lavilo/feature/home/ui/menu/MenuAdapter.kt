21382bc66238e5c86eedb847120d2908bc91f718
Ahmad N. Primordi <ahmadnuftahprimordi@gmail.com>
2020-03-05 17:10:30 +0700

    update menu adapter

--- app/src/main/java/com/makaryostudio/lavilo/feature/home/ui/menu/MenuAdapter.kt
+++ app/src/main/java/com/makaryostudio/lavilo/feature/home/ui/menu/MenuAdapter.kt
@@ -5,16 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.makaryostudio.lavilo.R
 import com.makaryostudio.lavilo.model.Food
+import kotlinx.android.synthetic.main.item_food.view.*
 
 class MenuAdapter(private val context: Context) : RecyclerView.Adapter<MenuAdapter.ViewHolder>() {
 
-    private var mListData: List<Food>? = null
+//    private var mListData: List<Food>? = null
+//
+//    internal fun setListData(mListData: List<Food>) {
+//        this.mListData = mListData
+//        notifyDataSetChanged()
+//    }
 
-    internal fun setListData(mListData: List<Food>) {
+    private var mListData = mutableListOf<Food>()
+
+    internal fun setListData(mListData: MutableList<Food>) {
         this.mListData = mListData
         notifyDataSetChanged()
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -24,17 +34,22 @@ class MenuAdapter(private val context: Context) : RecyclerView.Adapter<MenuAdapt
     }
 
     override fun getItemCount(): Int {
-        return mListData!!.size
+        return mListData.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+        val food = mListData[position]
+        holder.bindView(food)
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bindView(food: Food) {
-            Glide.w
+            Glide.with(context).load(food.imageFood).into(itemView.image_item_food_thumbnail)
+            itemView.title_item_food_name.text = food.nameFood
+            itemView.text_item_food_price.text = food.priceFood.toString()
+            itemView.text_item_food_quantity.text = food.quantityFood.toString()
+            itemView.text_item_food_stock.text = food.stockFood.toString()
         }
     }
 }
